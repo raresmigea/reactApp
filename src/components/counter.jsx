c7d62f6ff3fe98682c0acc38afb8d11d4fc7d2e3
raresmigea <raresini@yahoo.com>
2020-04-19 17:37:10 +0300

    added CSS style object

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,20 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {  //it's an obj that contains any data the component needs
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        // imageUrl: 'https://picsum.photos/200'
      };
+
+     style = { // its properties are CSS properties in cameCase
+        fontSize: 20,
+        fontWeight: 'bold'
+     };
+
     render() { 
         return (
             <React.Fragment>
-                <img src={this.state.imageUrl} alt=""></img>
-                <span>{this.formatCount()}</span>
-                <button>increment</button>
+                {/* <img src={this.state.imageUrl} alt=""></img> */}
+                <span style={this.style} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">increment</button>
             </React.Fragment>
         );
     }
