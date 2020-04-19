295d63025e2b732dde9952fe2a4d13d0d4c1b8d5
raresmigea <raresini@yahoo.com>
2020-04-19 17:32:05 +0300

    set attributes. add an image

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {  //it's an obj that contains any data the component needs
-        count: 0
+        count: 0,
+        imageUrl: 'https://picsum.photos/200'
      };
     render() { 
         return (
             <React.Fragment>
+                <img src={this.state.imageUrl} alt=""></img>
                 <span>{this.formatCount()}</span>
                 <button>increment</button>
             </React.Fragment>
