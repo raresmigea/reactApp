936e421057de5b3f3ff806406452bde4b3fe989f
raresmigea <raresini@yahoo.com>
2020-04-19 20:16:17 +0300

    event handling. increment count's value when button is clicked

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,21 +4,32 @@ class Counter extends Component {
     state = {  //it's an obj that contains any data the component needs
         count: 0,
         tags: ['tag1', 'tag2', 'tag3']
-     };
+    };
 
-     style = { // its properties are CSS properties in cameCase
+    style = { // its properties are CSS properties in cameCase
         fontSize: 20,
         fontWeight: 'bold'
-     };
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>no tags!</p>;
+        else return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
+    }
+    
+    //event handling: we use arrow functions because otherwise we need 
+    //to create a contructor() with super()
+    //and tell React about the changes with setState()
+    handleIncrement = () => {
+        this.state.count++;
+        this.setState({ coun: this.state.count + 1 });
+    }
 
     render() { 
         return (
             <React.Fragment>
                 <span style={this.style} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
+                <button onClick={this.handleIncrement.bind(this)} className="btn btn-secondary btn-sm">increment</button>
+                {this.renderTags()}
             </React.Fragment>
         );
     }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter  from './components/counter';
 
-const element = <h1>Hello world</h1>;
-
-ReactDOM.render(<Counter />, document.getElementById('root')); //1st: elemnt, 2nd: where in DOM to render it
+ReactDOM.render(<Counter />, document.getElementById('root')); //1st: element, 2nd: where in DOM to render it
