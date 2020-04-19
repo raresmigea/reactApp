8039bc0712cb2442523c58784e7426f1021ce1f7
raresmigea <raresini@yahoo.com>
2020-04-19 17:46:48 +0300

    Render listsdynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {  //it's an obj that contains any data the component needs
         count: 0,
-        // imageUrl: 'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
      };
 
      style = { // its properties are CSS properties in cameCase
@@ -16,6 +16,9 @@ class Counter extends Component {
             <React.Fragment>
                 <span style={this.style} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </React.Fragment>
         );
     }
