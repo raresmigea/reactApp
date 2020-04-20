b25ffc258648b5bd7f721569b2249ea2388b1e79
raresmigea <raresini@yahoo.com>
2020-04-20 20:11:15 +0300

    Removed the Local state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    //it's an obj that contains any data the component needs
-    value: this.props.counter.value,
-  };
-
   style = {
     // its properties are CSS properties in cameCase
     fontSize: 20,
     fontWeight: 'bold',
-  };
-
-  //event handling: we use arrow functions because otherwise we need
-  //to create a contructor() with super()
-  //and tell React about the changes with setState()
-  handleIncrement = () => {
-    this.state.value++;
-    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
@@ -28,7 +15,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className='btn btn-secondary btn-sm'
         >
           increment
@@ -45,12 +32,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = 'badge m-2 badge-';
-    classes += this.state.value === 0 ? 'warning' : 'primary';
+    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? 'Zero' : value;
   }
 }
