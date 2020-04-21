52296848a2e63a45b7e1c17f4517f9886e0777b3
raresmigea <raresini@yahoo.com>
2020-04-21 22:13:27 +0300

    changed input state

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Counters from './components/counters';
 class App extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 3, value: 3 },
+      { id: 4, value: 4 },
     ],
   };
 
