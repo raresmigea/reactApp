cddb792a2e58a861add326c7fc95340fef62eb33
raresmigea <raresini@yahoo.com>
2020-07-11 23:57:21 +0300

    added new data for the counters

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App extends Component {
       { id: 2, value: 2 },
       { id: 3, value: 3 },
       { id: 4, value: 4 },
+      { id: 5, value: 3 },
+      { id: 6, value: 2 },
+      { id: 7, value: 1 },
+      { id: 8, value: 5 },
     ],
   };
 
