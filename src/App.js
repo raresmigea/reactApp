6ab445109a6e3ebdfa5c7517fe8f51bccf2c7b9b
raresmigea <raresini@yahoo.com>
2020-11-14 23:54:23 +0200

    updated values

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       { id: 2, value: 2 },
       { id: 3, value: 3 },
       { id: 4, value: 4 },
-      { id: 5, value: 3 },
+      { id: 5, value: 6 },
       { id: 6, value: 1 },
       { id: 7, value: 1 },
     ],
