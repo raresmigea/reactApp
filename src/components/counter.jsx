ccc219a67f113cb6861fcf63d14652688efce8b9
raresmigea <raresini@yahoo.com>
2020-11-28 23:38:13 +0200

    added new line

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -21,6 +21,7 @@ class Counter extends Component {
   // this method is called before a component is removed from the DOM
   // gives opportunities for any type of cleanup before removal
   // otherwise could have memory leaks
+  
   componentWillUnmount() {
     console.log('counter - unmount');
   }
