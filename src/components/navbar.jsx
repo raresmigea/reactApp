0c0d7baf2717f233c8b8fed02edaa2fe796c0286
raresmigea <raresini@yahoo.com>
2020-12-14 23:57:40 +0200

    updated logs

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 // we get all data via props now
 // conver the component into -> stateles functional component
 // instead of using a class -> use a function
 
 const NavBar = ({ totalCounters }) => {
-  console.log('Navbar: rendered');
+  console.log('Navbar: Rendered');
   // cannot use life cycle hooks in stateless function components
   // because we have a single function that returns the output of the component
   // no need to use classes
 
-  return       (      
+  return (
     <nav className='navbar navbar-light nbg-light'>
       <a className='navbar-brand' href='#a'>
         Navbar
