25e067ef7d43d5a9b609a700dbdbc743652845f4
raresmigea <raresini@yahoo.com>
2021-02-27 00:28:40 +0200

    converted App component to functional component

--- src/App.js
+++ src/App.js
@@ -1,73 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import NavBar from './components/navbar';
 import Counters from './components/counters';
 
-class App extends Component {
-  state = {
-    counters: [
-      { id: 0, value: 5 },
-      { id: 1, value: 1 },
-      { id: 2, value: 2 },
-      { id: 3, value: 3 },
-      { id: 4, value: 4 },
-      { id: 5, value: 6 },
-      { id: 6, value: 1 },
-      { id: 7, value: 1 },
-    ],
+const App = () => {
+  const [counters, setCounters] = useState([
+    { id: 0, value: 5 },
+    { id: 1, value: 1 },
+    { id: 2, value: 2 },
+    { id: 3, value: 3 },
+    { id: 4, value: 4 },
+    { id: 5, value: 6 },
+    { id: 6, value: 1 },
+    { id: 7, value: 1 },
+  ]);
+
+  const handleDelete = (counterId) => {
+    const counterss = counters.filter((c) => c.id !== counterId);
+    setCounters(counterss);
   };
 
-  constructor(props) {
-    // called only once when an instance of a class is created
-    super(props); // need to pass props as a parameter
-    console.log('App: constructor', this.props);
-    // this.state instead of this.setState here
-  }
-
-  // it is called after the component is rendered into the DOM
-  // perfect place to make AJAX calls to get data from server
-  componentDidMount() {
-    console.log('App: mounted'); // called after render
-  }
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
+  const handleReset = () => {
+    const counterss = counters.map((c) => {
       c.value = 0;
       return c;
     });
-    this.setState({ counters });
+    setCounters(counterss);
   };
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    this.setState({ counters });
+  const handleIncrement = (counter) => {
+    const counterss = [...counters];
+    const index = counterss.indexOf(counter);
+    counterss[index] = { ...counter };
+    counterss[index].value++;
+    setCounters(counterss);
   };
-  render() {
-    console.log('App: rendered');
-    // when a component is rendered - all its children are rendered recursivelly
-    return (
-      <React.Fragment>
-        <NavBar
-          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+
+  return (
+    <React.Fragment>
+      <NavBar totalCounters={counters.filter((c) => c.value > 0).length} />
+      <main className='container'>
+        <Counters
+          counters={counters}
+          onReset={handleReset}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
         />
-        <main className='container'>
-          <Counters
-            counters={this.state.counters}
-            onReset={this.handleReset}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-          />
-        </main>
-      </React.Fragment>
-    );
-  }
-}
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default App;
