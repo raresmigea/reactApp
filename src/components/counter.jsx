4d5152080b2b368fd2713da107d3bed94fe3d2a5
raresmigea <raresini@yahoo.com>
2021-02-26 23:36:22 +0200

    converted Counter component to functional component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,66 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Counter extends Component {
-  style = {
+const Counter = ({ key, onDelete, onIncrement, counter }) => {
+  const style = {
     fontSize: 20,
     fontWeight: 'bold',
   };
 
-  // this method is called after a component is updated (new state/props)
-  // compares old one with new one.
-  // if there is a change:
-  // it could request data from the server
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('prevProps: ', prevProps);
-    console.log('prevState: ', prevState);
-    if (prevProps.counter.value !== this.props.counter.value) {
-      // could perform a call to the server
-    }
-  }
-
-  // this method is called before a component is removed from the DOM
-  // gives opportunities for any type of cleanup before removal
-  // otherwise could have memory leaks
-
-  componentWillUnmount() {
-    console.log('Counter: Unmount');
-  }
-
-  render() {
-    console.log('Counter: Rendered');
-    return (
-      <div>
-        {this.props.children}
-        <span style={this.style} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className='btn btn-secondary btn-sm'
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className='btn btn-danger btn-sm m-2'
-        >
-          Delete
-        </button>
-      </div>
-    );
-  }
-
-  getBadgeClasses() {
+  const getBadgeClasses = () => {
     let classes = 'badge m-2 badge-';
-    classes += this.props.counter.value === 0 ? 'warning' : 'primary';
+    classes += counter.value === 0 ? 'warning' : 'primary';
     return classes;
-  }
+  };
 
-  formatCount() {
-    const { value } = this.props.counter;
+  const formatCount = () => {
+    const { value } = counter;
     return value === 0 ? 'Zero' : value;
-  }
-}
+  };
+
+  return (
+    <div>
+      <span style={style} className={getBadgeClasses()}>
+        {formatCount()}
+      </span>
+      <button
+        onClick={() => onIncrement(counter)}
+        className='btn btn-secondary btn-sm'
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => onDelete(counter.id)}
+        className='btn btn-danger btn-sm m-2'
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default Counter;
