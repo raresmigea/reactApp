8623be74d2425a94a20ab93d91faec252a63654f
raresmigea <raresini@yahoo.com>
2021-04-10 23:44:13 +0300

    removed extra elements

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ const App = () => {
     { id: 0, value: 5 },
     { id: 1, value: 1 },
     { id: 2, value: 2 },
-    { id: 3, value: 3 },
-    { id: 4, value: 4 },
-    { id: 5, value: 6 },
-    { id: 6, value: 1 },
-    { id: 7, value: 1 },
   ]);
 
   const handleDelete = (counterId) => {
